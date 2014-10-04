dfb6f244cfa66a4a6848131ba83570470c45513e
Ashot K <ashot2005@yahoo.com>
2014-10-04 21:39:11 +0400

    With enum

---
+++ src/main/java/mkyong/user/Day.java
@@ -0,0 +1,9 @@
+package mkyong.user;
+
+/**
+ * Created by Ashot Karakhanyan on 04-10-2014
+ */
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+    THURSDAY, FRIDAY, SATURDAY
+}
--- src/main/java/mkyong/user/DropDownChoicePage.java
+++ src/main/java/mkyong/user/DropDownChoicePage.java
@@ -2,35 +2,38 @@ package mkyong.user;
 
 import java.util.Arrays;
 import java.util.List;
+
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 
 public class DropDownChoicePage extends WebPage {
 
-	private static final List<String> SEARCH_ENGINES = Arrays.asList("Google", "Bing", "Baidu");
+    //private static final List<String> SEARCH_ENGINES = Arrays.asList("Google", "Bing", "Baidu");
 
-	//make Google selected by default
-	private String selected = "Google";
-	public DropDownChoicePage(final PageParameters parameters) {
+    //make Google selected by default
+    private Day selectedDay = Day.MONDAY;
 
-		add(new FeedbackPanel("feedback"));
+    public DropDownChoicePage(final PageParameters parameters) {
 
-		DropDownChoice<String> listSites = new DropDownChoice<String>(
-				"sites", new PropertyModel<String>(this, "selected"), SEARCH_ENGINES);
+        add(new FeedbackPanel("feedback"));
 
-		Form<?> form = new Form<Void>("form") {
-			@Override
-			protected void onSubmit() {
-				info("Selected search engine : " + selected);
-			}
-		};
+        DropDownChoice<Day> listSites =
+                new DropDownChoice<Day>("sites", new Model<Day>(selectedDay), Arrays.asList(Day.values()));
 
-		add(form);
-		form.add(listSites);
+        Form<?> form = new Form<Void>("form") {
+            @Override
+            protected void onSubmit() {
+                info("Selected search engine : " + selectedDay);
+            }
+        };
 
-	}
+        add(form);
+        form.add(listSites);
+
+    }
 }
